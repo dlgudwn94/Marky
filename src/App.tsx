0a4abb477214603961e1e541d9e0a1d1e9503726
Hyungju <dlgudwn94>
2025-11-09 04:30:30 +0900

    [Modify]: 수정 기능 로직 변경으로 인한 파일 삭제 및 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AddBookmark from "./pages/AddBookmark";
-import EditBookmark from "./pages/EditBookmark";
 import Settings from "./pages/Settings";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/add" element={<AddBookmark />} />
-        <Route path="/edit/:id" element={<EditBookmark />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
